ab0c8e11d91f7a03b14fd7cf483e6a49a9e385d6
loloremi8 <remipetit@post.cz>
2024-11-03 22:30:57 +0100

    Basic start
    Created a placeholder

---
+++ InteractibleCharactersApp/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace InteractibleCharactersApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            // Position the window near the taskbar
+            PositionWindow();
+        }
+
+        private void PositionWindow()
+        {
+            // Example: place the window near the bottom-right corner of the screen
+            this.Left = SystemParameters.WorkArea.Width - this.Width - 20;
+            this.Top = SystemParameters.WorkArea.Height - this.Height - 20;
+        }
+    }
+}
